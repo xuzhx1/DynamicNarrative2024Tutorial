c2475d2a389797cf51f18006ec05942a961ed029
周尚礼 <105686154+zsl2021533013@users.noreply.github.com>
2024-03-27 21:09:43 +0800

    保存系统

---
+++ DynamicNarrative2024Tutorial/Assets/Scripts/Save System/SaveData_SO.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Save_System
+{
+    public class SaveData_SO : ScriptableObject
+    {
+        public Vector2 position;
+        public float energy;
+    }
+}
