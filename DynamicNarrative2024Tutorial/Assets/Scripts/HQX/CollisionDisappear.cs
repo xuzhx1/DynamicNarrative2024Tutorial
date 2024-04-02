6fa2ecfbc05eeb4ee1edf8ed96306b8757fdc973
EgilHu <qingccch@gmail.com>
2024-04-03 04:08:48 +0800

    HuQingxi_week6

---
+++ DynamicNarrative2024Tutorial/Assets/Scripts/HQX/CollisionDisappear.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CollisionDisappear : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
